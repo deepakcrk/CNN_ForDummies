a9f48912b0de4677bf89566439ec2c5e5c95c130
Deepak C R <deepakcrk@gmail.com>
2017-08-18 13:37:13 +0530

    Adding main.cpp..

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+  cout << "CNN..." << endl;
+  //if (...)
+  //train(configFile);
+
+  //if ()
+  //test( ...);
+}
+
