8b1b457d9573985fb43837d23f90bf8543850946
Deepak C R <deepakcrk@gmail.com>
2017-08-19 11:55:03 +0530

    added a struct to store layer params

---
+++ include/default_prototxt.h
@@ -0,0 +1,33 @@
+
+#include <iostream>
+// THIS IS INTENDED TO USE IF THERE IS NO PROTOTXT FILE SPECIFIED
+//
+
+
+enum LayerType
+{
+  CROP_LAYER = 1,
+  CONV_LAYER = 2,
+  POOL_LAYER = 3,
+  ACT_LAYER  =  4,
+  FC_LAYER   = 5,
+  NORM_LAYER = 6
+};
+
+
+
+struct layerParams
+{
+  //BASED ON THE LAYER TYPE USE PARAMETERS
+  enum LayerType type;
+  int outputs;
+  int windowSize  //use with pool aslo
+  double lr;
+  double decay;
+  double bias;    //used with other layers also
+  int stride;     //used with pool also
+  int type; //gaussian
+  int std;
+  int actFunc; //convert it in to enum for relu leaky relu etc.
+  int poolingType;
+};
