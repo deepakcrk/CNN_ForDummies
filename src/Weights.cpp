907f4c69955d387998c0c7ff0805c49486799e60
Deepak C R <deepakcrk@gmail.com>
2017-08-21 00:29:03 +0530

    Filter initialization modified

--- src/Weights.cpp
+++ src/Weights.cpp
@@ -6,26 +6,25 @@ void Weights::init(vector<LayerParams> layers)
 {
 
   m_layerParams = layers;
-  //Add above things in map
+
+  m_convWts.resize(layers.size());
+
   vector<pair<int, int> > windowOutput;
+  
   for (int i=0; i<layers.size(); i++)
   {
     if (layers[i].type == CONV_LAYER)
       windowOutput.push_back(make_pair(layers[i].windowSize, layers[i].outputs));
-  }
+    else
+      continue;
 
-  //FIXME WEIGHTS FOR FULLY CONNECTED NOT INITIALIZED. XXX NEED TO ADD THAT
 
-
-  //m_convWts
-  for (int i=0; i<windowOutput.size(); i++)
-  {
     vector<vector<Mat> > filts;
-    int totalFilters = windowOutput[i].second; //x3 incase of Color Image Now code is only for GRAY scale
+    int totalFilters = windowOutput[windowOutput.size()-1].second; //x3 incase of Color Image Now code is only for GRAY scale
     int depth = 1;
-    if (i != 0)
+    if (windowOutput.size() > 1)
     {
-      depth =windowOutput[i - 1].second;
+      depth =windowOutput[windowOutput.size() - 2].second;
     }
 
     for (int j=0; j<totalFilters; j++)
@@ -33,13 +32,13 @@ void Weights::init(vector<LayerParams> layers)
       vector<Mat> filts3d;
       for (int k=0; k<depth; k++)
       {
-        Mat filter1(windowOutput[i].first, windowOutput[i].first, CV_64FC1);
+        Mat filter1(windowOutput[windowOutput.size() - 1].first, windowOutput[windowOutput.size() - 1].first, CV_64FC1);
         randu (filter1, Scalar(-1.0), Scalar(1.0)); //FIXME make it double vals and make it Gaussian
         filts3d.push_back(filter1);
       }
       filts.push_back(filts3d);
     }
-    m_convWts.push_back(filts);
+    m_convWts[i] = filts;
   }
 }
 
