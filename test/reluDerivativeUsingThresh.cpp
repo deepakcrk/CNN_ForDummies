a80b60493510232e816ccde1d63ec2df8702d1a1
Deepak C R <deepakcrk@gmail.com>
2017-08-23 23:23:17 +0530

    adding a test program which does derivative of RELU using thresh

---
+++ test/reluDerivativeUsingThresh.cpp
@@ -0,0 +1,19 @@
+//Test program to check RELU is working or not
+#include <iostream>
+#include <vector>
+#include "opencv2/opencv.hpp"
+#include <stdio.h>
+using namespace std;
+using namespace cv;
+int main(int argc, const char** argv)
+{
+  Mat A = (Mat_<double>(3,3) << -3.5,-2.4,-1,  0,1.4,2.5,   3,4,-1);
+  
+  cout << "A = " << endl << " " << A << endl << endl;
+  Mat B;
+  threshold(A, B, 0, 1, THRESH_BINARY);
+
+  cout << "B = " << endl << " " << B << endl << endl;
+}
+
+
